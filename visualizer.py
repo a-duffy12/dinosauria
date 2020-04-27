386f3e132bf4c3642b79a8884b28d9a8c6fc1057
Aiden <12aduffy@gmail.com>
2020-04-26 20:11:14 -0400

    [Feat] Create simple visualization
    + Can now create simple visualizations
    + Two axes, wit major and minor gridlines
    + Colored points with outlines

--- visualizer.py
+++ visualizer.py
@@ -1,15 +1,21 @@
 from spreadsheet import data
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
 # marker size relative to genus size value
 volume = ()
 
 # generate simple scatter
-plot.xlabel('Time Existed (MYA)')
-plot.ylabel('Length (m)')
-plot.title('Size of Non-Avian Dinosaurs Over Time')
-plot.scatter(data.MYA, data.Size) #generates a plot
-plot.show() # displays the plot
+plt.xlabel('Time Existed (MYA)')
+plt.ylabel('Length (m)')
+plt.axis([240, 60, 0, 35])
+plt.title('Size of Non-Avian Dinosaurs Over Time')
+plt.scatter(data.MYA, data.Size, edgecolors='k') # generates a plot
+plt.set_axisbelow(True) # set axis below the points
+plt.grid(b=True, which='major', color='#777777', linestyle='-')
+plt.minorticks_on() # turn minor gridlines on
+plt.grid(b=True, which='minor', color='#aaaaaa', linestyle='dashed', alpha=0.5)
+plt.savefig('visualizations/dino_size_over_time.png', dpi=300) # save before show, or else a new fig is generated
+plt.show() # displays the plot
